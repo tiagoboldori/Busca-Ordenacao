94d437104a7825950321b626a4f014dcd450d410
tiagoboldori <158084889+tiagoboldori@users.noreply.github.com>
2024-08-15 21:21:13 -0300

    Add files via upload

---
+++ BinarySearch.py
@@ -0,0 +1,31 @@
+#Binary Search
+
+#dado o vetor:
+
+vetor=[1,5,8,9,12]
+
+def Bsort(v,x):
+    li=0
+    ls=len(v)-1
+    aux=0
+
+    while li<=ls:
+        aux=(li+ls)//2
+
+        if v[aux]<x:
+            li = aux+1
+
+        elif v[aux]>x:
+            ls = aux-1
+
+        else:
+            return aux
+        
+    return -1       
+        
+
+r = Bsort(vetor,int(input('valor a ser encontrado: ')))
+if r != -1:
+    print('valor no index:', r)
+else:
+    print('valor nao esta presente no vetor')

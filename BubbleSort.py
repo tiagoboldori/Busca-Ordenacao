a5a8e869d4d87631ce12d31c1f34a785780fc6cd
tiagoboldori <158084889+tiagoboldori@users.noreply.github.com>
2024-08-29 11:49:07 -0300

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -12,12 +12,23 @@ while True:
     count+=1
 
 def bubble(v):
+    cont=0
     for i in range(0, len(v)):
         for j in range(0, len(v)-1):
+            cont+=1
             if v[j]>v[j+1]:
                 v[j],v[j+1]=v[j+1],v[j]
-            
-    return v
+    return v,cont
+
+def betterBubble(v):
+    cont=0
+    for i in range(0,len(v)-1):
+        for j in range(len(v)-1,i,-1):
+            cont+=1
+            if v[j]<v[j-1]:
+                v[j],v[j-1]=v[j-1],v[j]
+    return v,cont
+
 
 print(bubble(vetor))
-
+print(betterBubble(vetor))
